6af8f25d621fe3a15e0e946b55432457bf68936c
unknown <jiang@jiang-PC.(none)>
2012-05-06 19:48:05 +0800

    新增 mastercode 内 beds_type 的 list列表 add编辑 del删除 add_act保存 功能

--- controllers/mastercode.php
+++ controllers/mastercode.php
@@ -7,6 +7,7 @@ class Mastercode extends IController
 {
 	public $layout='admin';
 	protected $checkRight = 'all';
+	public  $data =array();
 	
 	function init()
 	{
@@ -157,4 +158,83 @@ class Mastercode extends IController
 		$obj->del($where1);               //删除商品
 		$this->redirect('department_type_list');
 	}
+	/**
+	 * @床位模板数据配置
+	 */
+	//床位列表
+	function beds_list()
+	{
+		$this->redirect('beds_list');
+	}
+	//床位编辑
+	function beds_add()
+	{
+		$id = IReq::get('id','get');
+		//读取床位信息
+		if($id)
+		{
+			$beds_type_obj = new IModel('beds_type');
+			$where = "id =".$id;
+			$this->data['beds'] = $beds_type_obj->getObj($where);				
+		}
+		//加载医院信息
+		$hospital_obj = new IModel('hospital_detail');
+		
+		$temp =  $hospital_obj->query();
+
+		$this->data['hospital'] = $temp;
+		//加载科室信息
+		$department_obj = new IModel('department_type');
+		$this->data['department'] = $department_obj->query();
+		
+		$this->setRenderData($this->data);
+		$this->redirect('beds_add');
+	}
+	//床位保存
+	function beds_add_act()
+	{
+		$id = IReq::get('id');
+		$obj = new IModel('beds_type');
+		$dataarray= array(
+		'hospital_id' =>IReq::get('hospital_id'),
+		'department' =>IReq::get('department'),
+		'description_en' =>IReq::get('description_en'),
+		'description_ch' =>IReq::get('description_ch'),
+		'order' =>IReq::get('order'),
+		);
+		$obj->setData($dataarray);
+		//更新操作
+		if ($id)
+		{
+			$where = 'id ='.$id;
+			$obj->update($where);
+		}
+		//增加操作
+		else
+		{
+			$obj->add($where);
+		}
+		$this->redirect('beds_list');
+	}
+	//床位删除
+	function beds_del()
+	{
+		$id = IFilter::act( IReq::get('id') ,'int' );
+		
+		$obj = new IModel('beds_type');
+
+		if(is_array($id) && isset($id[0]) && $id[0]!='')
+		{
+			$id_str = join(',',$id);
+			$where1 = ' id in ('.$id_str.')';
+		}
+		else
+		{
+			$where1 = 'id = '.$id;
+		}
+		$obj->del($where1);               //删除商品
+		$this->redirect('beds_list');
+	}
+	
+	
 }
